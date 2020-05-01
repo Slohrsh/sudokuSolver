5495b04406eedc3559b357a2dcb4e9cd902a0fda
Slohrshi <sebastian.lohr@hs-weingarten.de>
2020-05-01 13:39:59 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SudokuGridComponent } from './sudoku-grid/sudoku-grid.component';
+import { SudokuCellComponent } from './sudoku-cell/sudoku-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SudokuGridComponent,
+    SudokuCellComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sudoku-grid/sudoku-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sudoku-grid',
+  templateUrl: './sudoku-grid.component.html',
+  styleUrls: ['./sudoku-grid.component.scss']
+})
+export class SudokuGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
